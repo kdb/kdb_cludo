9fe259cbcaf8ddcb40fc878a77840bae7a9fcf93
Benjamin Rasmussen <rasben@users.noreply.github.com>
2025-09-12 14:33:39 +0200

    Show page title as part of search breadcrumb. KBHBIB-92

--- src/Controller/CludoSearchBase.php
+++ src/Controller/CludoSearchBase.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\kdb_cludo\Controller;
 
+use Drupal\Core\Breadcrumb\Breadcrumb;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Link;
+use Drupal\Core\Path\PathValidatorInterface;
 use Drupal\dpl_breadcrumb\Services\BreadcrumbHelper;
 use Drupal\drupal_typed\DrupalTyped;
 use Drupal\kdb_cludo\CludoProfile;
@@ -28,10 +31,10 @@ abstract class CludoSearchBase extends ControllerBase {
   /**
    * Getting the title to be displayed in browser tab, and possibly on page.
    */
-  public function getTitle(): ?string {
+  public function getTitle(): string {
     $config = $this->profile->getConfigSettings();
 
-    return $config['title'] ?? NULL;
+    return $config['title'] ?? '';
   }
 
   /**
@@ -52,15 +55,30 @@ abstract class CludoSearchBase extends ControllerBase {
     $breadcrumb_node_id = $request->query->get('breadcrumb');
 
     if ($breadcrumb_node_id) {
-      $service = DrupalTyped::service(BreadcrumbHelper::class, 'dpl_breadcrumb.breadcrumb_helper');
+      ;
       $breadcrumb_node = $this->entityTypeManager()->getStorage('node')->load($breadcrumb_node_id);
 
       if ($breadcrumb_node instanceof NodeInterface) {
-        $breadcrumb = $service->getBreadcrumb($breadcrumb_node);
+        $breadcrumbHelper = DrupalTyped::service(BreadcrumbHelper::class, 'dpl_breadcrumb.breadcrumb_helper');
+        $breadcrumb = $breadcrumbHelper->getBreadcrumb($breadcrumb_node);
 
         if (empty($breadcrumb->getLinks())) {
           $breadcrumb->addLink($breadcrumb_node->toLink());
         }
+
+        $pathValidator = DrupalTyped::service(PathValidatorInterface::class, 'path.validator');
+        $currentUrl = $pathValidator->getUrlIfValid($request->getRequestUri());
+        $breadcrumb_links = $breadcrumb->getLinks();
+
+        if ($currentUrl) {
+          $breadcrumb_links = array_merge(
+            [Link::fromTextAndUrl($this->getTitle(), $currentUrl)],
+            $breadcrumb_links
+          );
+        }
+
+        $breadcrumb = (new Breadcrumb())->setLinks($breadcrumb_links);
+
       }
     }
 
